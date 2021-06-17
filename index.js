599470ad4a52a3932096e7f11f5a906ef912d8cb
Ruben Lazarus <49495427+BO5S5S@users.noreply.github.com>
2021-06-17 22:55:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,29 +3,37 @@ const prompt = require('prompt-sync')();
 let V = prompt();
 let N = prompt();
 let B=[];
-
-for(let i=0;i<N;i++){
-    B.push(prompt())
-}
-
-
-B.forEach(element=>{
-    if(check(element,V)){
-        console.log("Positive")
-    }else{
-        console.log("Negative")
+if(N>10){
+    console.log('maximum 10 peoples')
+}else{
+    for(let i=0;i<N;i++){
+        B.push(prompt())
     }
-})
-function check(Word,V){
-    let a= Word.length;
-    let b = V.length;
-    let i=0,j=0;
-    for(;i<a&&j<b;j++){
-      if(V[j]==Word[i])
-      {
-          i++;
-      }
+    
+    
+    B.forEach(element=>{
+        if(check(element,V)){
+            console.log("Positive")
+        }else{
+            console.log("Negative")
+        }
+    })
+    function check(Word,V){
+        
+        let a= Word.length;
+        let b = V.length;
+        let i=0,j=0;
+        if(a>b){
+    return false;
+        }
+        
+        for(;i<a&&j<b;j++){
+          if(V[j]==Word[i])
+          {
+              i++;
+          }
+        }
+        // console.log(i,j,a,b,Word,V);
+        return i==a;
     }
-    // console.log(i,j,a,b,Word,V);
-    return i==a;
 }
